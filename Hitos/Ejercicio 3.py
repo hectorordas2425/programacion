6cd5944e7cc834cac5016e921d311642de99cfc9
hectorordas2425 <hector.ordasrata24@campusfp.es>
2024-10-25 09:25:57 +0200

    Add files via upload

--- Hitos/Ejercicio 3.py
+++ Hitos/Ejercicio 3.py
@@ -1,14 +1,14 @@
-
+#Pedimos al usuario atraves de un while que ingrese su saldo y que le de error si su saldo es negativo para que le vuelva a preguntar
 while True:
     saldo=float(input("ingresar saldo de su cuenta: "))
     if saldo < 0:
         print("Error, el saldo no puede ser negativo")
     else:
         break
-
+#Iniciamos el numero de ingresos y de retiros en 0
 ingresos=0
 retiros=0
-
+#Creamos otro bucle while donde creamos una funcion para que nos muestre el menu
 while True:
     def menu():
         print("1. Ingresar dinero")
@@ -16,6 +16,7 @@ while True:
         print("3. Mostrar saldo")
         print("4. Estadísticas")
         print("5. Salir")
+#Pedimos al usuario que eliga una opcion, y que si esta entre el 1 y el 5 se salga del buchle while
     while True:
         opcion=int(input("Seleccionar una opcion: "))   
         if 1 <= opcion <=5:
@@ -23,7 +24,7 @@ while True:
         else:
             print("Error, introduzca una opción válida")
 
-
+#Si el usuario elige la opcion 1, le pedirá la cantidad que quiere ingresar y le mostrara el saldo final que tenga
     if opcion == 1:
         while True:
             cantidad=float(input("Ingresar cantidad de dinero: "))
@@ -34,7 +35,9 @@ while True:
                 ingresos += 1
                 print(f"Has ingresadoo: {cantidad}. Saldo actual: {saldo}")
             break
-
+#Si el usuario elige la opcion 2, le pedirá la cantidad que desea retirar de su saldo. 
+#La cantidad que desea retirar no puede ser mayor que el saldo que tenga, sino le dará error. 
+#La canntidad que introduzca se restará a su saldo
     elif opcion == 2:
         while True:
             cantidad=float(input("Ingresar cantidad de dinero: "))
@@ -47,12 +50,13 @@ while True:
                 retiros += 1
                 print(f"Has retirado: {cantidad}. Saldo actual: {saldo}")
                 break
-
+#Si el usuario elige la opcion 3, le mostrará el saldo actual que tenga
     elif opcion == 3:
         print("Tu saldo actual es: ",saldo)
-   
+#Si el usuario elige a opcion 4, le mostrará los ingresos y retiros totales realizados 
     elif opcion == 4:
         print(" Los ingresos realizados son: ", ingresos, "Los retiros realizados son: ",retiros)      
+#Si el usuario elige la opcion 5, se saldra del prgrama
     elif opcion == 5:
         print("Saliendo del programa") 
         break
--- Hitos/Ejercicio2.py
+++ Hitos/Ejercicio2.py
@@ -1,82 +1,91 @@
-#Muestra la opcion que eligira el usuario
+import random
+#Creamos una funcion para buscar el menu
 def opciones():
-    print("1-Piedra")
-    print("2-Papel")
-    print("3-Tijera")
-
-Def piedra()
-Def papel()
-Def tijera()
-
-
-
-
-
-
-
-
-opcion=0
-while opcion != 1 and opcion != 2 and opcion != 3:
-    opciones()
-    opcion= int(input("ingresar una opcion: ")) #Solicitamos al usuario que eliga una opción.
+    print("1-piedra")
+    print("2-papel")
+    print("3-tijera")
     
-    match opcion:
-        case 1:  #si introduce la opcion 1 (piedra)
-            piedra()
-        case 2:  #Si introduce la opcion 2 (papel)
-            papel()
-        case 3:  #Si introduce la opcion 3 (stijera) 
-            tijera()
-        case _:  #Si no introduce ninguna de las otras opciones
-            print("opcion incorrecta")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-while opcion != 1 and opcion !=2 and opcion != 3:
-    opcion_usuario()
-    opcion=int(input("Introduzca una opción"))
-    match opcion:
-        case 1:
-            print("Piedra")# Si introduce la opcion 1 (piedra)
+#Creamos una funcion para relacionar 1 = piedra, 2 = papel y 3 = tijera
+def eleccion(eleccion):
+    match eleccion:
+        case 1: 
+            return "piedra"
         case 2:
-            print("Papel")# Si introduce la opcion 2 (papel)
+            return "papel"
         case 3:
-            print("Tijera")# Si introduce la opcion 3 (tijera)
-        case _:
-            print("Opcion incorrecta")# Si no es ninguna de las otras opciones
+            return "tijera"
+        
+#Creamos otra funcion para que el usuario eliga una opcion del 1 al 3 y sino que le de error y vuelva a introducir la opcion
+def opcion_usuario():
+    while True:
+        opciones()
+        opcion=int(input("Elegir opcion (1,2,3): "))
+        if opcion in [1,2,3]:
+            return opcion
+        else:
+            print("Opcion incorrecta, porfavor eliga 1,2 o 3")
+    
+def casosPiedra(eleccionJugador, eleccionMaquina):
+    if eleccionJugador == 1 and eleccionMaquina == 3:
+        return "ganaste"
+    elif eleccionJugador == 1 and eleccionMaquina == 2:
+        return "perdiste"
+    else:
+        return "empate"
+    
+def casosPapel(eleccionJugador, eleccionMaquina):
+    if eleccionJugador == 2 and eleccionMaquina == 1:
+        return "ganaste"
+    elif eleccionJugador == 2 and eleccionMaquina == 3:
+        return "perdiste"
+    else:
+        return "empate"
 
+def casosTijera(eleccionJugador, eleccionMaquina):
+    if eleccionJugador == 3 and eleccionMaquina == 2:
+        return "ganaste"
+    elif eleccionJugador == 3 and eleccionMaquina == 1:
+        return "perdiste"
+    else:
+        return "empate"        
+    
+def juego():
+#iniciamos los puntos del usuario y de la maquina en 0
+    puntos_usuario=0
+    puntos_maquina=0
 
+#Creamos un bucle while para que mientras los puntos del usuario ni de la maquina sean 3 el juego siga adelante, y cuando el primer llegue a 3 puntos el juego termina
+    while puntos_usuario < 3 and puntos_maquina < 3: # puntos_maquina == 3 or puntos_usuario == 3 
+        obtener_opcion_usuario = opcion_usuario()
+        obtener_opcion_maquina = random.randint(1, 3)
+        
+        print("Tu elegiste: ", eleccion(obtener_opcion_usuario))
+        print("La maquina ha elegido: ", eleccion(obtener_opcion_maquina))
+        
+ #Muestra el resultado de cada ronda para saber si hemos perdido o ganado
+       
+        match obtener_opcion_usuario:
+            case 1:
+                resultado = casosPiedra(obtener_opcion_usuario, obtener_opcion_maquina)
+            case 2:
+                resultado = casosPapel(obtener_opcion_usuario, obtener_opcion_maquina)
+            case 3:
+                resultado = casosTijera(obtener_opcion_usuario, obtener_opcion_maquina)
+                
+        print("El resultado es: ",resultado)#Para mostrar si hemos ganado o perdido
+        match resultado:
+            case "ganaste":
+                puntos_usuario +=1
+            case "perdiste":
+                puntos_maquina +=1
+            case "empate":
+                puntos_usuario=0
+        print("Tu puntuacion es:",puntos_usuario, "Puntuacion de la maquina es:",puntos_maquina)
+#Establece un mensaje de animo o de felicitacion
+    if puntos_usuario == 3:
+            print("Felicidades, has ganado el juego")
+    else:
+            print("Lo siento, has perdido. Intentalo de nuevo")
 
-
-
-
-
-
+#Se ejecuta el juego
+juego()
